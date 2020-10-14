26c97fcc8276fa60dd56fef3c18044daf42e5356
Jayas <pzayazz@gmail.com>
2020-10-14 08:07:09 +0545

    Updated circular ll completely

--- DS/linkedList_circular.cpp
+++ DS/linkedList_circular.cpp
@@ -30,6 +30,8 @@ public:
     void display();
     void addInFront(Node *n);
     bool remove(int num);
+    Node *search(int value);
+    void addInMiddle(Node *prev, int num);
 };
 void printMenu();
 int main()
@@ -83,6 +85,38 @@ int main()
                 cout << "Number not deleted" << endl;
             }
             break;
+        case 5:
+            // Search node
+            cout << "Which number you want to search?" << endl;
+            cin >> number;
+            n1 = l1.search(number);
+            if (n1)
+            {
+                cout << "Node present" << endl;
+            }
+            else
+            {
+                cout << "Node not present" << endl;
+            }
+            break;
+        case 6:
+            // Insert in middle
+            cout << "Where do you want to insert?" << endl;
+            int prenum;
+            cin >> prenum;
+            n1 = l1.search(prenum);
+            if (n1 == NULL)
+            {
+                cout << "The number is not present " << endl;
+            }
+            else
+            {
+                cout << "Enter number " << endl;
+                cin >> number;
+                l1.addInMiddle(n1, number);
+                cout << "Insert Successful" << endl;
+            }
+            break;
         case 0:
             // Exiting
             loop = false;
@@ -96,10 +130,12 @@ int main()
 void printMenu()
 {
     cout << "******Menu******" << endl;
-    cout << "1. Insert" << endl;
+    cout << "1. Insert node" << endl;
     cout << "2. Display" << endl;
     cout << "3. Insert in the front" << endl;
-    cout << "4. Delete an item from the linked list" << endl;
+    cout << "4. Remove node" << endl;
+    cout << "5. Search node" << endl;
+    cout << "6. Insert node in the middle" << endl;
     cout << "0. Exit" << endl;
 }
 
@@ -174,8 +210,7 @@ bool LinkedList::remove(int num)
         {
             if (current->next->data == num)
             {
-                Node *tmp = current->next;
-                current->next = tmp->next;
+                current->next = current->next->next;
                 return true;
             }
             current = current->next;
@@ -189,4 +224,30 @@ bool LinkedList::remove(int num)
         }
     }
     return false;
+}
+Node *LinkedList::search(int value)
+{
+    if (last == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        Node *ptr = last;
+        do
+        {
+            if (ptr->data == value)
+            {
+                return ptr;
+            }
+            ptr = ptr->next;
+        } while (ptr != last);
+    }
+    return NULL;
+}
+void LinkedList::addInMiddle(Node *prev, int num)
+{
+    Node *n1 = new Node(num);
+    n1->next = prev->next;
+    prev->next = n1;
 }
