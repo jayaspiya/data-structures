c147c369746e90bd331187226dbeeaaccd52fd2d
Jayas <pzayazz@gmail.com>
2020-11-26 08:21:51 +0545

    Stack completed

--- DS/stack.cpp
+++ DS/stack.cpp
@@ -9,7 +9,7 @@ class Stack
 public:
     Stack(int size);
     void push(int x);
-    int pop();
+    void pop();
     int peek();
     int size();
     bool isEmpty();
@@ -38,9 +38,13 @@ int main()
     			break;
     		case 2:
     			// Pop
+    			stk.pop();
+    			cout << "Stack size is: " << stk.size() << endl;
     			break;
     		case 3:
     			// Peek
+    			num = stk.peek();
+    			cout << "Peeking : " << num << endl;
     			break;
     		case 4:
     			// Get Size of the stack
@@ -55,12 +59,14 @@ int main()
     }
 }
 void printMenu(){
-	cout << endl <<"Menu" << endl;
+	cout << endl;
+	cout <<"Menu" << endl;
 	cout << "1. Push an element to stack" << endl;
 	cout << "2. Pop an element from stack"<< endl;
 	cout << "3. Peek an element on stack" << endl;
 	cout << "4. Find size of stack" << endl;
 	cout << "0. Exit" << endl;
+	cout << endl;
 }
 Stack::Stack(int size)
 {
@@ -82,6 +88,16 @@ void Stack::push(int n){
 	
 }
 
+void Stack::pop(){
+	if(isEmpty()){
+		cout << "Cannot pop element. Stack empty" << endl;
+		return;
+	}
+	int poppedValue = *(base + top);
+	top--;
+	cout << poppedValue << " popped." << endl;
+}
+
 bool Stack::isFull(){
 	if(size() == total_capacity){
 		return true;
@@ -90,4 +106,18 @@ bool Stack::isFull(){
 }
 int Stack::size(){
 	return top + 1;
+}
+
+bool Stack::isEmpty(){
+	if(size() < 1){
+		return true;
+	}
+	return false;
+}
+
+int Stack::peek(){
+	if(isEmpty()){
+		cout << "Cannot peek. Stack empty." << endl;
+	}
+	return *(base + top);
 }
