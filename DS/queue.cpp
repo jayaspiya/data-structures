5f81bc3a1167d7c599c1b010443d3b6cb9e761a2
Jayas <pzayazz@gmail.com>
2020-11-26 08:23:56 +0545

    Queue.cpp added

---
+++ DS/queue.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+class Queue
+{
+	int *base;
+	int front;
+	int rear;
+	int total_capacity;
+public:
+	Queue(int size);
+	void enqueue(int x);
+	int dequeue();
+	int peek();
+	int size();
+	bool isEmpty();
+	bool isFull();
+};
+void printMenu();
+int main(){
+	int num, ch;
+	bool loop = true;
+	cout << "Enter the maximum capacity of the queue: " ;
+	cin >> num;
+	Queue queue(num);
+	while(loop){
+		printMenu();
+		cout << "Enter your choice: " ;
+		cin >> ch;
+		switch(ch){
+			case 1:
+				cout << "Enter the element to enqueue: ";
+				cin >> num;
+				queue.enqueue(num);
+				cout << "Queue size: " << queue.size() << endl;
+				break;
+			case 2:
+				num = queue.dequeue();
+				cout << num << " dequeued" << endl;
+				cout << "Queue size: " << queue.size() << endl;
+				break;
+			case 0:
+				break;
+		}
+	}
+}
+void printMenu(){
+	cout << endl;
+	cout << "Menu" << endl;
+	cout << "1. Enqueue an element" << endl;
+	cout << "2. Dequece an element" << endl;
+	cout << "3. Peek an element" << endl;
+	cout << "4. Size of the queue" << endl;
+	cout << "0. Exit" << endl;
+	cout << endl;
+}
+Queue::Queue(int size){
+	base = new int[size];
+	total_capacity = size;
+	front = 0;
+	rear = -1;
+};
+
+void Queue::enqueue(int num){
+	// Check if the queue is full
+	if(isFull()){
+		cout << "Cannot enqueue. Queue full" << endl;
+		return;
+	}
+	rear++;
+	*(base + rear) = num;
+	
+}
+int Queue::dequeue(){
+	if(isEmpty()){
+		cout << "Cannot dequeue. Queue Empty" << endl;
+		return 0;
+	}
+	int dequeue_num = *(base + front);
+	front++;
+	return dequeue_num;
+}
+bool Queue::isFull(){
+	if (size() >= total_capacity){
+		return true;
+	}
+	return false;
+}
+bool Queue::isEmpty(){
+	if(front >= rear){
+		return true;
+	}
+	return false;
+}
+int Queue::size(){
+	return (rear - front + 1);
+}
