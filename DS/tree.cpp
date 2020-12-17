8d28decd6250e6ab76e1e3e70c255bb40d8e23b4
Jayas <pzayazz@gmail.com>
2020-12-17 16:41:37 +0545

    Node insert and display added

--- DS/tree.cpp
+++ DS/tree.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Node{
+public:
 	int data;
 	Node *right;
 	Node *left;
@@ -15,20 +16,30 @@ class Node{
 		left = NULL;
 		right = NULL;
 	}
-}
+};
 class BinaryTree{
 public:
 	Node *root;
 	void insert(int num);
 	void insert(int num, Node *leaf);
+	void print2D();
+	void print2DUtil(Node *leaf, int space);
 	
-}
+};
 void printMenu(){
 	cout << "Menu" << endl;
 	cout << "Add new node" << endl;
 }
 int main(){
-    
+	BinaryTree *binaryTree = new BinaryTree();
+	binaryTree->insert(45);
+	binaryTree->insert(43);
+	binaryTree->insert(65);
+	binaryTree->insert(35);
+	binaryTree->insert(15);
+	binaryTree->insert(20);
+	binaryTree->print2D();
+	
 }
 void BinaryTree::insert(int num){
 	if(root == NULL){
@@ -57,4 +68,21 @@ void BinaryTree::insert(int num, Node *leaf){
 	}
 }
 
+void BinaryTree::print2D(){
+	// Pass initial space count as 0
+	print2DUtil(root, 0);
+}
 
+void BinaryTree::print2DUtil(Node *leaf, int space){
+	if(leaf == NULL){
+		return;
+	}
+	space += 5;
+	print2DUtil(leaf->right, space);
+	cout<<endl;
+	for(int i = 5;i < space; i++){
+		cout<<" ";
+	}
+	cout << leaf->data << endl;
+	print2DUtil(leaf->left, space);
+}
